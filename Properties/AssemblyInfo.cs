e3f77f29656fb65eb88e6783a19eff40a94a1705
Alexander Sergeev <sergeevalexander@hotmail.com>
2021-02-01 17:27:11 +0300

    Мелкие правки

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.5")]
-[assembly: AssemblyFileVersion("3.0.0.5")]
+[assembly: AssemblyVersion("3.0.0.7")]
+[assembly: AssemblyFileVersion("3.0.0.7")]
 [assembly: NeutralResourcesLanguage("ru-RU")]
