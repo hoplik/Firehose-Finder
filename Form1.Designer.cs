ca3e1085421ed0ef48460a29d85fb1da8711a052
Alexander Sergeev <sergeevalexander@hotmail.com>
2021-02-15 20:38:05 +0300

    Немного поправил дизайн, добавил новые устройства

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -77,6 +77,9 @@
             this.Column_Comp = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel1 = new System.Windows.Forms.Panel();
             this.groupBox_tm_model = new System.Windows.Forms.GroupBox();
+            this.label_model = new System.Windows.Forms.Label();
+            this.label_altname = new System.Windows.Forms.Label();
+            this.label_tm = new System.Windows.Forms.Label();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.radioButton_adb_reset = new System.Windows.Forms.RadioButton();
             this.radioButton_man_reset = new System.Windows.Forms.RadioButton();
@@ -84,9 +87,6 @@
             this.label_log = new System.Windows.Forms.Label();
             this.checkBox_Log = new System.Windows.Forms.CheckBox();
             this.button_findIDs = new System.Windows.Forms.Button();
-            this.label_altname = new System.Windows.Forms.Label();
-            this.label_model = new System.Windows.Forms.Label();
-            this.label_tm = new System.Windows.Forms.Label();
             this.label_SW_Ver = new System.Windows.Forms.Label();
             this.label_swid = new System.Windows.Forms.Label();
             this.label_hwid = new System.Windows.Forms.Label();
@@ -178,7 +178,7 @@
             this.справкаToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1369, 30);
+            this.menuStrip1.Size = new System.Drawing.Size(1369, 28);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -187,7 +187,7 @@
             this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.выходToolStripMenuItem});
             this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
-            this.файлToolStripMenuItem.Size = new System.Drawing.Size(59, 26);
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
             this.файлToolStripMenuItem.Text = "Файл";
             // 
             // выходToolStripMenuItem
@@ -207,7 +207,7 @@
             this.toolStripSeparator2,
             this.внестиПроизводителяМодельToolStripMenuItem});
             this.видToolStripMenuItem.Name = "видToolStripMenuItem";
-            this.видToolStripMenuItem.Size = new System.Drawing.Size(49, 26);
+            this.видToolStripMenuItem.Size = new System.Drawing.Size(49, 24);
             this.видToolStripMenuItem.Text = "Вид";
             // 
             // приветствиеToolStripMenuItem
@@ -266,7 +266,7 @@
             this.вопросОтветToolStripMenuItem,
             this.оПрограммеToolStripMenuItem});
             this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
-            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(81, 26);
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
             this.справкаToolStripMenuItem.Text = "Справка";
             // 
             // вопросОтветToolStripMenuItem
@@ -450,7 +450,7 @@
             this.tabPage_firehose.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tabPage_firehose.Name = "tabPage_firehose";
             this.tabPage_firehose.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tabPage_firehose.Size = new System.Drawing.Size(1361, 545);
+            this.tabPage_firehose.Size = new System.Drawing.Size(1361, 547);
             this.tabPage_firehose.TabIndex = 0;
             this.tabPage_firehose.Text = "Работа с файлами";
             this.tabPage_firehose.UseVisualStyleBackColor = true;
@@ -462,7 +462,7 @@
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel2.Location = new System.Drawing.Point(3, 151);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1355, 362);
+            this.panel2.Size = new System.Drawing.Size(1355, 364);
             this.panel2.TabIndex = 18;
             // 
             // textBox_main_term
@@ -472,7 +472,7 @@
             this.textBox_main_term.Multiline = true;
             this.textBox_main_term.Name = "textBox_main_term";
             this.textBox_main_term.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox_main_term.Size = new System.Drawing.Size(409, 362);
+            this.textBox_main_term.Size = new System.Drawing.Size(409, 364);
             this.textBox_main_term.TabIndex = 16;
             // 
             // dataGridView_final
@@ -498,7 +498,7 @@
             this.dataGridView_final.RowHeadersWidth = 51;
             this.dataGridView_final.RowTemplate.Height = 24;
             this.dataGridView_final.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView_final.Size = new System.Drawing.Size(946, 362);
+            this.dataGridView_final.Size = new System.Drawing.Size(946, 364);
             this.dataGridView_final.TabIndex = 15;
             this.dataGridView_final.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView_final_CellClick);
             this.dataGridView_final.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView_final_CellDoubleClick);
@@ -592,6 +592,33 @@
             this.groupBox_tm_model.TabStop = false;
             this.groupBox_tm_model.Text = "Для устройства";
             // 
+            // label_model
+            // 
+            this.label_model.AutoSize = true;
+            this.label_model.Location = new System.Drawing.Point(178, 21);
+            this.label_model.Name = "label_model";
+            this.label_model.Size = new System.Drawing.Size(23, 17);
+            this.label_model.TabIndex = 22;
+            this.label_model.Text = "---";
+            // 
+            // label_altname
+            // 
+            this.label_altname.AutoSize = true;
+            this.label_altname.Location = new System.Drawing.Point(382, 21);
+            this.label_altname.Name = "label_altname";
+            this.label_altname.Size = new System.Drawing.Size(23, 17);
+            this.label_altname.TabIndex = 23;
+            this.label_altname.Text = "---";
+            // 
+            // label_tm
+            // 
+            this.label_tm.AutoSize = true;
+            this.label_tm.Location = new System.Drawing.Point(6, 21);
+            this.label_tm.Name = "label_tm";
+            this.label_tm.Size = new System.Drawing.Size(23, 17);
+            this.label_tm.TabIndex = 21;
+            this.label_tm.Text = "---";
+            // 
             // groupBox4
             // 
             this.groupBox4.Controls.Add(this.radioButton_adb_reset);
@@ -665,33 +692,6 @@
             this.button_findIDs.Text = "Опросить устройство с перезагрузкой в аварийный режим";
             this.button_findIDs.UseVisualStyleBackColor = true;
             this.button_findIDs.Click += new System.EventHandler(this.Button_findIDs_Click);
-            // 
-            // label_altname
-            // 
-            this.label_altname.AutoSize = true;
-            this.label_altname.Location = new System.Drawing.Point(382, 21);
-            this.label_altname.Name = "label_altname";
-            this.label_altname.Size = new System.Drawing.Size(23, 17);
-            this.label_altname.TabIndex = 23;
-            this.label_altname.Text = "---";
-            // 
-            // label_model
-            // 
-            this.label_model.AutoSize = true;
-            this.label_model.Location = new System.Drawing.Point(178, 24);
-            this.label_model.Name = "label_model";
-            this.label_model.Size = new System.Drawing.Size(23, 17);
-            this.label_model.TabIndex = 22;
-            this.label_model.Text = "---";
-            // 
-            // label_tm
-            // 
-            this.label_tm.AutoSize = true;
-            this.label_tm.Location = new System.Drawing.Point(6, 21);
-            this.label_tm.Name = "label_tm";
-            this.label_tm.Size = new System.Drawing.Size(23, 17);
-            this.label_tm.TabIndex = 21;
-            this.label_tm.Text = "---";
             // 
             // label_SW_Ver
             // 
@@ -824,7 +824,7 @@
             this.toolStripStatusLabel_filescompleted,
             this.toolStripProgressBar_filescompleted,
             this.toolStripStatusLabel_vol});
-            this.statusStrip_firehose.Location = new System.Drawing.Point(3, 513);
+            this.statusStrip_firehose.Location = new System.Drawing.Point(3, 515);
             this.statusStrip_firehose.Name = "statusStrip_firehose";
             this.statusStrip_firehose.Padding = new System.Windows.Forms.Padding(1, 0, 13, 0);
             this.statusStrip_firehose.Size = new System.Drawing.Size(1355, 30);
@@ -1133,11 +1133,11 @@
             this.tabControl1.Controls.Add(this.tabPage_firehose);
             this.tabControl1.Controls.Add(this.tabPage_collection);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControl1.Location = new System.Drawing.Point(0, 30);
+            this.tabControl1.Location = new System.Drawing.Point(0, 28);
             this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1369, 574);
+            this.tabControl1.Size = new System.Drawing.Size(1369, 576);
             this.tabControl1.TabIndex = 0;
             // 
             // bindingSource_collection
