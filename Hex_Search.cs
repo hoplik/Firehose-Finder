2b3d41f1a2ed17f9d6bfb8671f73fb8735c886f6
Alexander Sergeev <sergeevalexander@hotmail.com>
2021-12-18 00:25:21 +0300

    Доработки поиска

--- Hex_Search.cs
+++ Hex_Search.cs
@@ -30,6 +30,23 @@ namespace FirehoseFinder
         private void Button_start_search_Click(object sender, EventArgs e)
         {
             Dictionary<string, long> filestosearch = new Dictionary<string, long>();
+            if (backgroundWorker_hex_search.IsBusy)
+            {
+                //Если паралельный поток ещё выполняется, предлагаем остановить
+                if (MessageBox.Show("Осторожно, вы предпринимаете попытку остановить длительную процедуру поиска" +
+                    " Нажимая кнопку \"Ok\", вы подтверждаете выполнение операции.",
+                    "Внимание, остановка длительной операции!",
+                    MessageBoxButtons.OKCancel,
+                    MessageBoxIcon.Warning,
+                    MessageBoxDefaultButton.Button2,
+                    MessageBoxOptions.DefaultDesktopOnly) == DialogResult.OK)
+                {
+
+                    backgroundWorker_hex_search.CancelAsync();
+                    toolStripStatusLabel_search.Text = "Текущий поиск отменён пользователем.";
+                    toolStripProgressBar_search.Value = 0;
+                }
+            }
             listView_hex_search.Items.Clear();
             toolStripStatusLabel_search.Text = string.Empty;
             if (textBox_hexsearch.Text.Length % 2 != 0) textBox_hexsearch.Text = "0" + textBox_hexsearch.Text;
@@ -57,22 +74,18 @@ namespace FirehoseFinder
                 {
                     inputsearch.Add(filename.Key);
                 }
-                if (backgroundWorker_hex_search.IsBusy)
-                {
-                    //Если поток ещё выполняется, предлагаем его остановить
-                }
-                else
-                {
-                    Search_Hex sh = new Search_Hex(inputsearch, textBox_hexsearch.Text); //Создаём сущность Словарь+поиск и её отправляем в поток
-                    backgroundWorker_hex_search.RunWorkerAsync(sh);
-                }
+                //Создаём сущность Словарь+поиск и её отправляем в поток
+                backgroundWorker_hex_search.RunWorkerAsync(new Search_Hex(inputsearch, textBox_hexsearch.Text));
             }
         }
 
         private void BackgroundWorker_hex_search_DoWork(object sender, DoWorkEventArgs e)
         {
+            BackgroundWorker worker = sender as BackgroundWorker;
             Search_Hex hex_search = (Search_Hex)e.Argument;
             List<Search_Result> addr_value_file = new List<Search_Result>();
+            int countfile = 1;
+            int currfiles = hex_search.FullFileNames.Count;
             foreach (string fullfilename in hex_search.FullFileNames)
             {
                 FileInfo fi = new FileInfo(fullfilename);
@@ -84,7 +97,18 @@ namespace FirehoseFinder
                     var chunk = new byte[hex_search.SearchString.Length / 2];
                     using (var stream = File.OpenRead(fullfilename))
                     {
-                        //int byteschunk = stream.Read(chunk, 0, 4);
+                        for (int i = 0; i < fullfilename.Length; i++)
+                        {
+                            if (string.Format("{0:X2}", stream.ReadByte()).Equals(hex_search.SearchString.Substring(0, 2)))
+                            {
+                                sr.Adress_hex = i.ToString();
+                            }
+                            else
+                            {
+
+                            }
+                        }
+                        int byteschunk = stream.Read(chunk, 0, 4);
                         //for (int i = 0; i < byteschunk; i++) dumptext.Insert(i * 2, string.Format("{0:X2}", (int)chunk[i]));
                         //if (Enum.IsDefined(typeof(Guide.FH_magic_numbers), Convert.ToUInt32(dumptext.ToString(), 16)))
                         //{
@@ -94,10 +118,11 @@ namespace FirehoseFinder
                         //    for (int i = 0; i < byteschunk; i++) dumptext.Insert(i * 2, string.Format("{0:X2}", (int)chunk[i]));
                         //}
                     }
+                    sr.Result_String = "****" + hex_search.SearchString + "****";
+                    addr_value_file.Add(sr);
+                    worker.ReportProgress(countfile * 100 / currfiles, string.Format("{0} из {1}", countfile, currfiles));
+                    countfile++;
                 }
-                sr.Adress_hex = "0x10000";
-                sr.Result_String = "****" + hex_search.SearchString + "****";
-                addr_value_file.Add(sr);
             }
             e.Result = addr_value_file;
         }
@@ -120,6 +145,7 @@ namespace FirehoseFinder
                         hsearchres.SubItems.Add(sr.File_Name);
                         listView_hex_search.Items.Add(hsearchres);
                     }
+                    toolStripStatusLabel_search.Text = string.Format("Найдено {0} совпадений в {1} файлах", result.Count, "всех");
                 }
                 else
                 {
