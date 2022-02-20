3b8b6b8882e7e43f530b56c3cb411013f168e38f
Alexander Sergeev <sergeevalexander@hotmail.com>
2022-02-20 19:53:48 +0300

    Добавлены новые устройства

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("22.2.9.0")]
-[assembly: AssemblyFileVersion("22.2.9.0")]
+[assembly: AssemblyVersion("22.2.20.0")]
+[assembly: AssemblyFileVersion("22.2.20.0")]
 [assembly: NeutralResourcesLanguage("ru-RU")]
